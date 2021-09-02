568d27a6ac40effcf88659b5a52265ecd9c10233
Natanael <natanaelsantosbr@gmail.com>
2021-09-01 21:40:56 -0300

    Ajustes

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! 2");
+            Console.WriteLine("Hello World! 3");
         }
     }
 }
