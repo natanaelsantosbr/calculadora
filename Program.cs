62b8a7891bbe96cb0219f13545b1c9854ce13d47
Natanael <natanaelsantosbr@gmail.com>
2021-09-01 21:55:49 -0300

    Utilizando funcoes

--- Program.cs
+++ Program.cs
@@ -6,6 +6,11 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
+            Soma();
+        }
+
+        static void Soma()
+        {
             Console.Clear();
             System.Console.WriteLine("Primeiro valor: ");
             float v1 = float.Parse(Console.ReadLine());
@@ -18,12 +23,12 @@ namespace Calculator
             float resultado = v1 + v2;
             System.Console.WriteLine("O resultado da soma é: " + resultado);
 
-            //interpolação de string
-            System.Console.WriteLine($"O resultado da soma é: {resultado}");
+            // //interpolação de string
+            // System.Console.WriteLine($"O resultado da soma é: {resultado}");
 
-            System.Console.WriteLine($"O resultado da soma é: {v1 + v2}");
+            // System.Console.WriteLine($"O resultado da soma é: {v1 + v2}");
 
-            System.Console.WriteLine($"O resultado da soma é: " + (v1 + v2));
+            // System.Console.WriteLine($"O resultado da soma é: " + (v1 + v2));
         }
     }
 }
